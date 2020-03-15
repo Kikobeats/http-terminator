ecff3eed7adf9ad68b3ba14bf2a02b6edd8bab84
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-03-15 18:15:53 +0100

    build: next

--- src/factories/createHttpTerminator.js
+++ src/factories/createHttpTerminator.js
@@ -7,9 +7,5 @@ import type {
 import createInternalHttpTerminator from './createInternalHttpTerminator';
 
 export default (configurationInput: HttpTerminatorConfigurationInputType): HttpTerminatorType => {
-  const httpTerminator = createInternalHttpTerminator(configurationInput);
-
-  return {
-    terminate: httpTerminator.terminate,
-  };
+  return createInternalHttpTerminator(configurationInput);
 };
